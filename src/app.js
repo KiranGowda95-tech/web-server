8d565472274d1fb717182575fb0888ee1d8f40ec
Kiran <kirankumardn74@gmail.com>
2024-06-19 18:07:17 +0530

    files updated

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
+const path = require('path');
 const express = require('express');
 
+console.log(__dirname);
+console.log();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
 const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'weather app',
+    name: 'Kiran',
+  });
+});
 
 app.get('', (req, res) => {
   res.send('hello express');
 });
-app.get('/help', (req, res) => {
-  res.send('welocome to help page');
-});
-app.get('/about', (req, res) => {
-  res.send('welcome to About page ');
-});
+
 app.get('/weather', (req, res) => {
-  res.send('welcome to weather page');
+  res.send({
+    forcast: 'its raining',
+    location: 'bangalore',
+  });
 });
 
 app.listen(3000, () => {
